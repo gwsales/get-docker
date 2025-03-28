63f3a256b2aa12864ea5106eeee6fa6e4b7263b5
gwsales <grantwsales@gmail.com>
2025-03-28 13:56:34 +0000

    update docker install with no net script

---
+++ install-no-net.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+which docker 2>/dev/null
+if [[ $? -ne 0 ]]; then
+    ## Docker not installed
+    if [[ ! -f get-docker.sh ]]; then
+        curl -fsSL https://get.docker.com -o get-docker.sh
+        sudo sh get-docker.sh
+        sudo usermod -aG docker $USER
+        if [[ ! -f /etc/docker/daemon.json ]]; then
+            sudo cp daemon.json /etc/docker/daemon.json
+        fi
+    else
+        echo "[warn] get-docker.sh might have alrady ran"
+    fi
+else
+    echo "[info] Docker already installed"
+fi
+
