3befa956cb9fec172e2ee33eebcec32765f43f7d
gwsales <grantwsales@gmail.com>
2026-03-10 05:39:40 -0500

    updated with data-root option

--- install-no-net.sh
+++ install-no-net.sh
@@ -2,19 +2,14 @@
 
 which docker 2>/dev/null
 if [[ $? -ne 0 ]]; then
-    ## Docker not installed
-    if [[ ! -f get-docker.sh ]]; then
-        curl -fsSL https://get.docker.com -o get-docker.sh
-        sudo sh get-docker.sh
-        sudo usermod -aG docker $USER
-        if [[ ! -f /etc/docker/daemon.json ]]; then
-            sudo cp daemon.json /etc/docker/daemon.json
-        fi
-	rm get-docker.sh
-    else
-        echo "[warn] install-docker.sh might have alrady ran"
-    fi
+  echo "[info] Installing docker."
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sudo sh get-docker.sh
+  sudo usermod -aG docker $USER
+  rm get-docker.sh
+  sudo cp no-net.json /etc/docker/daemon.json
 else
-    echo "[info] Docker already installed"
+  echo "[info] Docker already installed."
 fi
-
+echo "[info] Done."
+exit 0
--- install.sh
+++ install.sh
@@ -2,14 +2,19 @@
 
 which docker 2>/dev/null
 if [[ $? -ne 0 ]]; then
-    ## Docker not installed
-    if [[ ! -f get-docker.sh ]]; then
-        curl -fsSL https://get.docker.com -o get-docker.sh
-        sudo sh get-docker.sh
-        sudo usermod -aG docker $USER
-    else
-        echo "[warn] get-docker.sh might have alrady ran"
-    fi
+  echo "[info] Installing docker."
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sudo sh get-docker.sh
+  sudo usermod -aG docker $USER
+  rm get-docker.sh
+  if [[ -d /docker ]]; then
+    echo "[info] Setting data-root to /docker vs /var/lib/docker."
+    sudo cp data-root.json /etc/docker/daemon.json
+  else
+    echo "[info] Using default data-root /var/lib/docker."
+  fi
 else
-    echo "[info] Docker already installed"
+  echo "[info] Docker already installed."
 fi
+echo "[info] Done."
+exit 0
